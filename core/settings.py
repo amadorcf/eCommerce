220d474a8e8c6d8b510c7094fdff9b85b5c8331b
amadorcf <amadorcf@outlook.com>
2022-10-25 09:53:03 +0200

    Revert "Insert STATICFILES_DIRS route"
    This reverts commit a9d78dce134f38744c55bee96cdfad004e8b6dbf.

--- core/settings.py
+++ core/settings.py
@@ -126,10 +126,6 @@ STATIC_URL = 'static/'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
-STATICFILES_DIRS = [
-    BASE_DIR / "static",
-    '/var/www/static/',
-]
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 

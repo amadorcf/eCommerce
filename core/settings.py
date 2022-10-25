b61ae87859b1546c4ebd0b8e30380f656c1c2672
amadorcf <amadorcf@outlook.com>
2022-10-25 10:54:13 +0200

    Solve some issues in models

--- core/settings.py
+++ core/settings.py
@@ -128,4 +128,4 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
-DEFAULT_AUTO_FIELD = 'django.db.models.py.BigAutoField'
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'

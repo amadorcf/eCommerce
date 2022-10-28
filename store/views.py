be59ae0dfc28c447840fde7ac910c56d1967faa9
amadorcf <amadorcf@outlook.com>
2022-10-28 12:51:06 +0200

    Some modifitcations

--- store/views.py
+++ store/views.py
@@ -2,15 +2,15 @@ from django.shortcuts import render, get_object_or_404
 from .models import Category, Product
 
 
-
-def all_products(request):
-    products = Product.objects.all()
+def product_all(request):
+    # Show the available products
+    products = Product.objects.filter(is_active=True)
     return render(request, 'store/home.html', {'products': products})
 
 
 def product_detail(request, slug):
     product = get_object_or_404(Product, slug=slug, in_stock=True)
-    return render(request, 'store/products/detail.html', {'product': product})
+    return render(request, 'store/products/product_detail.html', {'product': product})
 
 
 def category_list(request, category_slug):
